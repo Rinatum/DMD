fc92c9b9bf1d323836948e251ea00322b2f4c940
Rinatum <grinamul@gmail.com>
2018-11-22 12:46:23 +0300

    cars dataset

--- config.py
+++ config.py
@@ -13,6 +13,11 @@ def generate_number(length):
 
 """ ***************** Cars ***************** """
 
+
+def get_random_email():
+    return generate_string(2) + generate_number(3) + generate_string(1) + '18'
+
+
 cars = {'colors': ['black', 'white', 'red', 'green', 'blue', 'orange'],
         'socket_types': [0, 1, 2],
         'models': ['lada', 'bmw', 'mercedes', 'mitsubishi', 'hyundai', 'tesla'],
@@ -26,8 +31,7 @@ cars = {'colors': ['black', 'white', 'red', 'green', 'blue', 'orange'],
 
 customers = {'username': generate_string(8),
              'payment_Info': generate_number(16),
-             'location': 'id of location'
-             }
+             'Location': [-1]}  # get_location() }
 
 """ ***************** Personal Info ***************** """
 
@@ -61,11 +65,23 @@ provider = {'id_provider': generate_number(8),
 
 """ ***************** Provider_has_Detail ***************** """
 
-provider_has_detail = {'id_provider': 'id of provider',
-                       'id_detail_list': 'id of detailList',
-                       'price': generate_number(3)
-                       }
+""" ***************** Employers ***************** """
 
 """ ***************** Detail_type ***************** """
 
 detail_type = {''}
+""" ***************** Buildings & ChargingStation ***************** """
+
+
+def get_id():
+    return random.randint(0, 1000)
+
+
+def get_charging_time():
+    return random.randint(20, 50)
+
+
+def get_cost():
+    return random.randint(200, 1000)
+
+
