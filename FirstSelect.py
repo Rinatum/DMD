6a2f17b5cd02a6455419e3b868069a73f53653c2
Rinatum <grinamul@gmail.com>
2018-11-23 11:42:19 +0300

    database init

--- FirstSelect.py
+++ FirstSelect.py
@@ -3,8 +3,6 @@ import main
 data = main.connect()
 print(data)
 
-
-
 date = '2018-04-12'
 username = 'keksik'
 lp_begin = 'AN'
@@ -12,6 +10,5 @@ lp_begin = 'AN'
 select = "SELECT * FROM " \
          "Cars C JOIN " \
          "Custimer Cu JOIN Order O ON " \
-         "Cu.username = '"+username+"' and O.StartTime between '"+date +"' and '"+date+" 23:59:59'"\
-         "WHERE C.color = 'red' and SUBSTRING(c.LicensePlate,1,2)='"+lp_begin+"'"
-
+         "Cu.username = '" + username + "' and O.StartTime between '" + date + "' and '" + date + " 23:59:59'" \
+                                                                                                  "WHERE C.color = 'red' and SUBSTRING(c.LicensePlate,1,2)='" + lp_begin + "'"
