7e07f3fd3bee8ef1f1d503f07c1ab58f7434c187
SimonWT <y.yudinskih@innopolis.ru>
2018-11-26 02:40:06 +0300

    Select #1,3,7,9 Pure SQL Backup

--- ThirdSelect.py
+++ ThirdSelect.py
@@ -6,87 +6,26 @@ data = main.call_procedure('select_query_3', date)
 print(data)
 
 
-#Pure SQL
-# CREATE
-# DEFINER = `katya` @ ` % ` PROCEDURE `
-# select_query_3
-# `(IN fist_day_of_the_week VARCHAR(45))
+#CREATE DEFINER=`katya`@`%` PROCEDURE `select_query_3`(IN fist_day_of_the_week VARCHAR(45))
 # BEGIN
 #
-# SELECT
-# COUNT(*) * 100
-# DIV((SELECT COUNT( *)
-# FROM
-# Vehicle) *7) AS
-# Count, 'Morning'
-# AS
-# 'Time'
-# FROM
-# `Order`
-# JOIN
-# Vehicle
-# WHERE((HOUR(StartTime) between 7 and 10)
-# OR(HOUR(FinishTime)
-# between
-# 7 and 10))
-# and ((DATE(StartTime) between fist_day_of_the_week and DATE(fist_day_of_the_week) + 7)
-# OR(DATE(FinishTime)
-# between
-# fist_day_of_the_week and DATE(fist_day_of_the_week) + 7))
-# GROUP
-# BY
-# Vehicle.licensePlate
+# 	SELECT COUNT(*) * 100 DIV ((SELECT COUNT(*) FROM Vehicle) * 7) AS Count, 'Morning' AS 'Time' FROM `Order` JOIN Vehicle
+#     WHERE ((HOUR(StartTime) between 7 and 10) OR (HOUR(FinishTime) between 7 and 10))
+#     and ((DATE(StartTime) between fist_day_of_the_week and DATE(fist_day_of_the_week)+7) OR (DATE(FinishTime) between fist_day_of_the_week and DATE(fist_day_of_the_week)+7))
+#     GROUP BY Vehicle.licensePlate
 #
-# UNION
+#     UNION
 #
-# SELECT
-# COUNT(*) * 100
-# DIV((SELECT COUNT( *)
-# FROM
-# Vehicle) *7) AS
-# Count, 'Afternoon'
-# AS
-# 'Time'
-# FROM
-# `Order`
-# JOIN
-# Vehicle
-# WHERE((HOUR(StartTime) between 12 and 14)
-# OR(HOUR(FinishTime)
-# between
-# 12 and 14))
-# and ((DATE(StartTime) between fist_day_of_the_week and DATE(fist_day_of_the_week) + 7)
-# OR(DATE(FinishTime)
-# between
-# fist_day_of_the_week and DATE(fist_day_of_the_week) + 7))
-# GROUP
-# BY
-# Vehicle.licensePlate
+#     SELECT COUNT(*) * 100 DIV ((SELECT COUNT(*) FROM Vehicle) * 7) AS Count, 'Afternoon' AS 'Time' FROM `Order` JOIN Vehicle
+#     WHERE ((HOUR(StartTime) between 12 and 14) OR (HOUR(FinishTime) between 12 and 14))
+#     and ((DATE(StartTime) between fist_day_of_the_week and DATE(fist_day_of_the_week)+7) OR (DATE(FinishTime) between fist_day_of_the_week and DATE(fist_day_of_the_week)+7))
+#     GROUP BY Vehicle.licensePlate
 #
-# UNION
+#     UNION
 #
-# SELECT
-# COUNT(*) * 100
-# DIV((SELECT COUNT( *)
-# FROM
-# Vehicle) *7) AS
-# Count, 'Evening'
-# AS
-# 'Time'
-# FROM
-# `Order`
-# JOIN
-# Vehicle
-# WHERE((HOUR(StartTime) between 17 and 19)
-# OR(HOUR(FinishTime)
-# between
-# 17 and 19))
-# and ((DATE(StartTime) between fist_day_of_the_week and DATE(fist_day_of_the_week) + 7)
-# OR(DATE(FinishTime)
-# between
-# fist_day_of_the_week and DATE(fist_day_of_the_week) + 7))
-# GROUP
-# BY
-# Vehicle.licensePlate;
+#     SELECT COUNT(*) * 100 DIV ((SELECT COUNT(*) FROM Vehicle) * 7) AS Count, 'Evening' AS 'Time' FROM `Order` JOIN Vehicle
+#     WHERE ((HOUR(StartTime) between 17 and 19) OR (HOUR(FinishTime) between 17 and 19))
+#     and ((DATE(StartTime) between fist_day_of_the_week and DATE(fist_day_of_the_week)+7) OR (DATE(FinishTime) between fist_day_of_the_week and DATE(fist_day_of_the_week)+7))
+#     GROUP BY Vehicle.licensePlate;
 #
 # END
