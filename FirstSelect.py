4e7926f74874b68331e38799969452460b851e5d
SimonWT <y.yudinskih@innopolis.ru>
2018-11-25 12:35:56 +0300

    First select *edit

--- FirstSelect.py
+++ FirstSelect.py
@@ -6,7 +6,7 @@ username = 'keksik'
 lp_begin = 'AN'
 color = 'red'
 
-select = f"SELECT * FROM Customer JOIN PersonalInfo ON PersonalInfo.Name='{username}' JOIN `Order` ON `Order`.StartTime between '{date}' and '{date} 23:59:59' JOIN Vehicle WHERE Vehicle.Color = '{color}'"
+select = f"SELECT * FROM Customer JOIN PersonalInfo ON PersonalInfo.Usename='{username}' JOIN `Order` ON `Order`.StartTime between '{date}' and '{date} 23:59:59' JOIN Vehicle WHERE Vehicle.Color = '{color}'"
 
 #SELECT * FROM Customer JOIN PersonalInfo ON PersonalInfo.Name='Rinat' JOIN `Order` ON `Order`.StartTime between '2017-01-29 14:54:00' and '2017-01-29 14:54:00' JOIN Vehicle WHERE Vehicle.Color = 'red'
 
